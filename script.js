5add45c7104bac97b607784b7d08449e204f6b33
Pola Someshwar <57764440+polasomeshwar@users.noreply.github.com>
2022-10-09 18:16:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    document.querySelector("#converter").addEventListener("submit",(event)=>{
+        event.preventDefault();
+        const {target :{from,to,amount}} = event;
+        let headers = new Headers();
+        headers.append("apikey","DcL793uYNT0ZG7xGj5HVg26928PVbytm");
+        let requestOptions = {
+            method : "GET",
+            headers,
+        }
+        fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.value}`,requestOptions)
+        .then(response=>response.json())
+        .then(data=>{
+            let {from,to,result} = data;
+            document.querySelector("#resu").textContent = `After Conversion = ${result}`;
+        });
+    })
+});
